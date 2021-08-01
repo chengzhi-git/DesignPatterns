05093e458ce1425d02df721f24bee9ae88e16e5e
Roy <2890008985@qq.com>
2021-08-01 22:14:02 +0800

    创建Dog、Cat类实现Compareble接口

--- src/Strategy/Main.java
+++ src/Strategy/Main.java
@@ -1,6 +1,7 @@
 package Strategy;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 /**
  * @Author:chengzhi
@@ -10,7 +11,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Compareble[] a = {new Dog(3), new Dog(5), new Dog(1)};
-        Sorter.sort(a);
+        //Sorter.sort(a);
         System.out.println(Arrays.toString(a));
+
+        Comparetor<Integer> com = (x, y) -> {
+            System.out.println("");
+            return Integer.compare(x, y);
+        };
     }
 }
